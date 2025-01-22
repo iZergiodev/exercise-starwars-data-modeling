6eac0f257a4846fef076c086bc758a080e7b097c
iZergiodev <155020065+iZergiodev@users.noreply.github.com>
2025-01-22 13:44:10 +0000

    new

--- src/models.py
+++ src/models.py
@@ -7,26 +7,59 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    email = Column(String(250))
+    password = Column(String(250))
+
+
+    
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(250))
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+    height = Column(String(250))
+    birth_year = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    description = Column(String(250))
+    diameter = Column(Integer)
+    climate = Column(Integer)
+    gravity = Column(Integer)
+    terrain = Column(String(250))
+    population = Column(Integer)
 
-    def to_dict(self):
-        return {}
+class Vehicle(Base):
+    __tablename__ = 'vehicle' 
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250))
+    vehicle_class = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(Integer)
+    length = Column(Integer)
+    crew = Column(Integer)
+    passengers = Column(Integer)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    character = relationship(Character)
+    planet = relationship(Planet)
+    vehicle = relationship(Vehicle)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
